2d598531ffcf841f3ad0c3ba9c4352d0bb5d9bec
alexanderreimeriu <alexander.reimer@iu-study.org>
2025-05-29 15:32:11 +0200

    second version

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
-function showStars() {
-  const container = document.getElementById('stars-container');
-  container.innerHTML = '';
+const starNames = [
+  "Путь", "Сущность", "Карма", "Дар", "Задача", "Опора",
+  "Память", "Предназначение", "Сила Рода", "Боль Души",
+  "Обретение Себя", "РОКОВАЯ ОШИБКА", "Будущее", "Служение"
+];
 
-  const starNames = [
-    "Путь", "Сущность", "Карма", "Дар", "Задача", "Опора", 
-    "Память", "РОКОВАЯ ОШИБКА", "Будущее", "Служение"
+function calculateMatrix() {
+  const birthInput = document.getElementById("birthdate").value;
+  const starsContainer = document.getElementById("stars");
+  starsContainer.innerHTML = "";
+
+  if (!birthInput) {
+    alert("Пожалуйста, введите дату.");
+    return;
+  }
+
+  const birthDate = new Date(birthInput);
+  const numbers = [
+    birthDate.getDate(),
+    birthDate.getMonth() + 1,
+    birthDate.getFullYear()
   ];
 
-  for (let name of starNames) {
-    const star = document.createElement('div');
-    star.classList.add('star');
-    
-    if (name === "РОКОВАЯ ОШИБКА") {
-      star.classList.add('special');
+  const reduced = numbers.flatMap(num => num.toString().split('').map(Number));
+  const total = reduced.reduce((a, b) => a + b, 0);
+
+  for (let i = 0; i < starNames.length; i++) {
+    const star = document.createElement("div");
+    star.classList.add("star");
+    star.innerText = starNames[i];
+
+    if (starNames[i] === "РОКОВАЯ ОШИБКА") {
+      star.classList.add("special");
+      starsContainer.appendChild(star);
+      break;
     }
 
-    star.innerText = name;
-    container.appendChild(star);
-
-    if (name === "РОКОВАЯ ОШИБКА") break;
+    starsContainer.appendChild(star);
   }
 }
